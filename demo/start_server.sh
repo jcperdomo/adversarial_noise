64826c8012e05d6a1fcfa8c0879c030e4daf4d04
w4ngatang <wang.alex.c@gmail.com>
2017-07-24 16:56:27 +0000

    Add option to run locally

--- demo/start_server.sh
+++ demo/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+RUN_LOCAL=${1:-"1"}
+
 #python -m demo/main --data_path src/data/cifar10 --load_model_from src/checkpoints/06-26/cifar10.ckpt --n_modules 5 --n_kerns 128 --eps .1
-python -m demo/main --data_path src/data/facescrub_d128 --load_model_from src/checkpoints/07-10/facescrub_d128.ckpt --n_modules 7 --n_kerns 128 --eps .1
+python -m demo/main --data_path src/data/facescrub_d128 --load_model_from src/checkpoints/07-10/facescrub_d128.ckpt --n_modules 7 --n_kerns 128 --eps .1 --run_local $RUN_LOCAL
